12e4807952834d80f029a4e238bf63f48a086be5
Taiel Alen Nunes <taielalen56@hotmail.com>
2024-10-07 08:06:53 -0300

    agrego funcionalidad para ingresar tareas

---
+++ index.js
@@ -0,0 +1,51 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const tasks = []
+
+const menu = () => {
+  console.log('=======================')
+  console.log('    Lista de Tareas    ')
+  console.log('=======================')
+  console.log('1: Agergar Tarea')
+  console.log('2: Listar tareas')
+  console.log('3: Actualizar Tareas')
+  console.log('4: Eliminar Tareas')
+  console.log('5: Salir')
+
+  rl.question('Selecciona una opcion: ', (option) => {
+    switch (option) {
+      case '1':
+        addTask()
+        break
+
+      case '5':
+        console.log('nos vemos!!!')
+        rl.close()
+        break
+
+      default:
+        console.log('opcion invalida')
+        menu()
+        break
+    }
+  })
+
+  console.log(tasks[0])
+  console.log(tasks[1])
+}
+
+// Agregar tareas: Escribir nuevas tareas en un archivo JSON.
+const addTask = () => {
+  rl.question('ingrese su tarea:\n', (task) => {
+    tasks.push(task)
+    console.log('tarea agregada correctamente')
+    menu()
+  })
+}
+
+menu()
